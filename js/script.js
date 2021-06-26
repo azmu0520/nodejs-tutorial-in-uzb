4cb74f1e078a1e7a7c118bbeefd7ed3c16d11503
sardor.mukhtorov <sardor.mukhtorov@inoviagroup.se>
2021-06-26 23:57:42 +0500

    new way of accessing

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ let chooseBtn = document.getElementById("choose"),
   receiveBtn = document.getElementById("receive"),
   text = document.getElementsByTagName("h2")[0];
 
-function hover() {
-  text.textContent = "All In All";
-}
+text.addEventListener("mouseenter", function () {
+  text.textContent = "All in All";
+});
