df41d851d2dc98ace4e647a06c418968bd1d60db
sardor.mukhtorov <sardor.mukhtorov@inoviagroup.se>
2021-06-27 00:15:29 +0500

    modal part added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 let chooseBtn = document.getElementById("choose"),
   receiveBtn = document.getElementById("receive"),
-  text = document.getElementsByTagName("h2")[0];
+  text = document.getElementsByTagName("h2")[0],
+  modal = document.querySelectorAll(".modal")[0];
+exit = document.querySelectorAll(".close")[0];
 
+function hover() {
+  text.textContent = "All in All";
+}
 text.addEventListener("mouseenter", function () {
   text.textContent = "All in All";
+});
+
+text.addEventListener("mouseleave", function () {
+  text.textContent = "All inclusive";
+});
+
+receiveBtn.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+
+exit.addEventListener("click", function () {
+  modal.style.display = "none";
 });
