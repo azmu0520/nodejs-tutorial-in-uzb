adee497f7af5e88d17cd8ee43a3e6db0591c2593
sardor.mukhtorov <sardor.mukhtorov@inoviagroup.se>
2021-06-26 23:26:08 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let chooseBtn = document.getElementById("choose"),
+  receiveBtn = document.getElementById("receive"),
+  text = document.getElementsByTagName("h2")[0];
+
+function hover() {
+  text.textContent = "All In All";
+}
