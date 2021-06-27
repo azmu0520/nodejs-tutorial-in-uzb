96b3af58dfcb3ceb36dba86279511824c7c70ae0
sardor.mukhtorov <sardor.mukhtorov@inoviagroup.se>
2021-06-27 15:17:27 +0500

    name , phone , and mail inputs actived

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 let chooseBtn = document.getElementById("choose"),
   receiveBtn = document.getElementById("receive"),
   text = document.getElementsByTagName("h2")[0],
-  modal = document.querySelectorAll(".modal")[0];
-exit = document.querySelectorAll(".close")[0];
+  modal = document.querySelectorAll(".modal")[0],
+  exit = document.querySelectorAll(".close")[0],
+  name = document.getElementsByName("name")[0],
+  phone = document.getElementsByName("phone")[0],
+  mail = document.getElementsByName("mail")[0],
+  message = document.getElementsByName("message")[0];
 
 function hover() {
   text.textContent = "All in All";
@@ -21,4 +25,16 @@ receiveBtn.addEventListener("click", function () {
 
 exit.addEventListener("click", function () {
   modal.style.display = "none";
+});
+
+name.addEventListener("input", function () {
+  message.value = `My name is ${name.value} `;
+});
+
+phone.addEventListener("input", function () {
+  message.value = `My phone number is ${phone.value} `;
+});
+
+mail.addEventListener("input", function () {
+  message.value = `My gmail is ${mail.value} `;
 });
